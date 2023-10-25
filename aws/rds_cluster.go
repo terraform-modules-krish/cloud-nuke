25409eb7e98e7292fce7a8a3220d3290681a726e
repo-copier <info@gruntwork.io>
2023-10-24 18:55:15 -0600

    Resolve references

--- aws/rds_cluster.go
+++ aws/rds_cluster.go
@@ -7,8 +7,8 @@ import (
 	awsgo "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/rds"
-	"github.com/gruntwork-io/cloud-nuke/logging"
-	"github.com/gruntwork-io/gruntwork-cli/errors"
+	"github.com/terraform-modules-krish/cloud-nuke/logging"
+	gruntwork-cli "github.com/terraform-modules-krish/go-commons/errors"
 )
 
 func waitUntilRdsClusterDeleted(svc *rds.RDS, input *rds.DescribeDBClustersInput) error {
