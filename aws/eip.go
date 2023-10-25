737c5074136c0dea78ec5b455a93393c675b87b2
repo-copier <info@gruntwork.io>
2023-10-24 18:55:33 -0600

    Resolve references

--- aws/eip.go
+++ aws/eip.go
@@ -6,8 +6,8 @@ import (
 	awsgo "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/gruntwork-io/cloud-nuke/logging"
-	"github.com/gruntwork-io/gruntwork-cli/errors"
+	"github.com/terraform-modules-krish/cloud-nuke/logging"
+	gruntwork-cli "github.com/terraform-modules-krish/go-commons/errors"
 )
 
 func setFirstSeenTag(svc *ec2.EC2, address ec2.Address, key string, value time.Time, layout string) error {
