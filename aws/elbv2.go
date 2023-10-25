dc31b3cf28f713acd93c1005fa1ca3d0a41c8c14
repo-copier <info@gruntwork.io>
2023-10-24 18:55:58 -0600

    Resolve references

--- aws/elbv2.go
+++ aws/elbv2.go
@@ -6,9 +6,9 @@ import (
 	awsgo "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/elbv2"
-	"github.com/gruntwork-io/cloud-nuke/config"
-	"github.com/gruntwork-io/cloud-nuke/logging"
-	"github.com/gruntwork-io/go-commons/errors"
+	"github.com/terraform-modules-krish/cloud-nuke/config"
+	"github.com/terraform-modules-krish/cloud-nuke/logging"
+	"github.com/terraform-modules-krish/go-commons/errors"
 )
 
 // Returns a formatted string of ELBv2 Arns
