79ca50240afb65de3fe4d9feee7ba4cf8f54e9ef
repo-copier <info@gruntwork.io>
2023-10-24 18:55:43 -0600

    Resolve references

--- aws/launch_config.go
+++ aws/launch_config.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/autoscaling"
-	"github.com/gruntwork-io/cloud-nuke/logging"
-	"github.com/gruntwork-io/go-commons/errors"
+	"github.com/terraform-modules-krish/cloud-nuke/logging"
+	"github.com/terraform-modules-krish/go-commons/errors"
 )
 
 // Returns a formatted string of Launch config Names
