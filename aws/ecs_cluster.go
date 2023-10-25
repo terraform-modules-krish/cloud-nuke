fe1d5dac96061738e8e93daad634be101c08d073
repo-copier <info@gruntwork.io>
2023-10-24 18:56:04 -0600

    Resolve references

--- aws/ecs_cluster.go
+++ aws/ecs_cluster.go
@@ -7,9 +7,9 @@ import (
 	awsgo "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ecs"
-	"github.com/gruntwork-io/cloud-nuke/config"
-	"github.com/gruntwork-io/cloud-nuke/logging"
-	"github.com/gruntwork-io/go-commons/errors"
+	"github.com/terraform-modules-krish/cloud-nuke/config"
+	"github.com/terraform-modules-krish/cloud-nuke/logging"
+	"github.com/terraform-modules-krish/go-commons/errors"
 )
 
 // Used in this context to determine if the ECS Cluster is ready to be used & tagged
