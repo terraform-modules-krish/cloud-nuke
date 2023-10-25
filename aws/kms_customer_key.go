af479108274ce17db85247a3f8cb7aba3afa107e
repo-copier <info@gruntwork.io>
2023-10-24 18:55:50 -0600

    Resolve references

--- aws/kms_customer_key.go
+++ aws/kms_customer_key.go
@@ -7,8 +7,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/kms"
-	"github.com/gruntwork-io/cloud-nuke/logging"
-	"github.com/gruntwork-io/go-commons/errors"
+	"github.com/terraform-modules-krish/cloud-nuke/logging"
+	"github.com/terraform-modules-krish/go-commons/errors"
 	"github.com/hashicorp/go-multierror"
 )
 
