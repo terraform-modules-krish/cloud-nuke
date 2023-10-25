f46e8748461bfcf4f3672a3d046cfe96375abb84
repo-copier <info@gruntwork.io>
2023-10-24 18:56:18 -0600

    Resolve references

--- aws/iam_group.go
+++ aws/iam_group.go
@@ -1,18 +1,18 @@
 package aws
 
 import (
-	"github.com/gruntwork-io/cloud-nuke/report"
-	"github.com/gruntwork-io/cloud-nuke/telemetry"
-	commonTelemetry "github.com/gruntwork-io/go-commons/telemetry"
+	"github.com/terraform-modules-krish/cloud-nuke/report"
+	"github.com/terraform-modules-krish/cloud-nuke/telemetry"
+	commonTelemetry "github.com/terraform-modules-krish/go-commons/telemetry"
 	"sync"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/iam"
-	"github.com/gruntwork-io/cloud-nuke/config"
-	"github.com/gruntwork-io/cloud-nuke/logging"
-	"github.com/gruntwork-io/gruntwork-cli/errors"
+	"github.com/terraform-modules-krish/cloud-nuke/config"
+	"github.com/terraform-modules-krish/cloud-nuke/logging"
+	gruntwork-cli "github.com/terraform-modules-krish/go-commons/errors"
 	"github.com/hashicorp/go-multierror"
 )
 
--- logging/logger.go
+++ logging/logger.go
@@ -1,6 +1,6 @@
 package logging
 
-import "github.com/gruntwork-io/go-commons/logging"
+import "github.com/terraform-modules-krish/go-commons/logging"
 
 // Logger - Global logger variable
 var Logger = logging.GetLogger("cloud-nuke", "")
