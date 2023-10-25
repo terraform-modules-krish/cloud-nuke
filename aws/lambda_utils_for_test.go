9fb222354d94258dce3a31bbe0a5dc34ce2eaa3d
repo-copier <info@gruntwork.io>
2023-10-24 18:55:58 -0600

    Resolve references

--- aws/lambda_utils_for_test.go
+++ aws/lambda_utils_for_test.go
@@ -6,7 +6,7 @@ import (
 	awsgo "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/iam"
-	gruntworkerrors "github.com/gruntwork-io/go-commons/errors"
+	gruntworkerrors "github.com/terraform-modules-krish/go-commons/errors"
 	"github.com/stretchr/testify/assert"
 )
 
