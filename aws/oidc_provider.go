4424a7be11b3b618d4627b77de587b18632b221b
repo-copier <info@gruntwork.io>
2023-10-24 18:55:54 -0600

    Resolve references

--- aws/oidc_provider.go
+++ aws/oidc_provider.go
@@ -8,9 +8,9 @@ import (
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/iam"
-	"github.com/gruntwork-io/cloud-nuke/config"
-	"github.com/gruntwork-io/cloud-nuke/logging"
-	"github.com/gruntwork-io/go-commons/errors"
+	"github.com/terraform-modules-krish/cloud-nuke/config"
+	"github.com/terraform-modules-krish/cloud-nuke/logging"
+	"github.com/terraform-modules-krish/go-commons/errors"
 	"github.com/hashicorp/go-multierror"
 )
 
