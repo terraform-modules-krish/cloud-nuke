80adef6c212b77b06c6dee59edde7fc02159094d
repo-copier <info@gruntwork.io>
2023-10-24 18:55:48 -0600

    Resolve references

--- aws/launch_config_test.go
+++ aws/launch_config_test.go
@@ -7,8 +7,8 @@ import (
 	awsgo "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/autoscaling"
-	"github.com/gruntwork-io/cloud-nuke/util"
-	"github.com/gruntwork-io/go-commons/errors"
+	"github.com/terraform-modules-krish/cloud-nuke/util"
+	"github.com/terraform-modules-krish/go-commons/errors"
 	"github.com/stretchr/testify/assert"
 )
 
