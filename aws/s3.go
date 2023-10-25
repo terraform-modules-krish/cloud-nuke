d83c97c3c95ff4271a416e83dbd164524496a090
repo-copier <info@gruntwork.io>
2023-10-24 18:55:14 -0600

    Resolve references

--- aws/s3.go
+++ aws/s3.go
@@ -14,9 +14,9 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/gruntwork-io/cloud-nuke/config"
-	"github.com/gruntwork-io/cloud-nuke/logging"
-	"github.com/gruntwork-io/gruntwork-cli/errors"
+	"github.com/terraform-modules-krish/cloud-nuke/config"
+	"github.com/terraform-modules-krish/cloud-nuke/logging"
+	gruntwork-cli "github.com/terraform-modules-krish/go-commons/errors"
 )
 
 // getS3BucketRegion returns S3 Bucket region.
